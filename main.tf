8d9a3f70c81260f25c3e66c3c3e3efad005f0630
Mark Gray (MSFT) <2914336+grayzu@users.noreply.github.com>
2020-06-04 14:40:01 -0700

    Added a few resources and connected to TF cloud

--- main.tf
+++ main.tf
@@ -1,8 +1,22 @@
 terraform {
+  backend "remote" {
+    organization = "OSS-integrations"
 
+    workspaces {
+      name = "personal-pg"
+    }
+  }
 }
 
 resource "azurerm_resource_group" "demo01" {
-    name                      = "PP-RedisEnt-test"
-    location                  = "eastus2"
+  name                      = "PP-RedisEnt-test"
+  location                  = "eastus2"
+}
+
+resource "azurerm_container_registry" "demo01" {
+  name                     = "demoRegistry01"
+  resource_group_name      = azurerm_resource_group.demo01.name
+  location                 = azurerm_resource_group.demo01.location
+  sku                      = "Basic"
+  admin_enabled            = false
 }
