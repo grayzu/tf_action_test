1c4d2480ba553314a5c6c3419ebf47c7a49fc354
Mark Gray <mark,gray@microsoft.com>
2020-06-04 15:34:45 -0700

    fixed formatting

--- main.tf
+++ main.tf
@@ -9,14 +9,14 @@ terraform {
 }
 
 resource "azurerm_resource_group" "demo01" {
-  name                      = "PP-RedisEnt-test"
-  location                  = "eastus2"
+  name     = "PP-RedisEnt-test"
+  location = "eastus2"
 }
 
 resource "azurerm_container_registry" "demo01" {
-  name                     = "demoRegistry01"
-  resource_group_name      = azurerm_resource_group.demo01.name
-  location                 = azurerm_resource_group.demo01.location
-  sku                      = "Basic"
-  admin_enabled            = false
+  name                = "demoRegistry01"
+  resource_group_name = azurerm_resource_group.demo01.name
+  location            = azurerm_resource_group.demo01.location
+  sku                 = "Basic"
+  admin_enabled       = false
 }
