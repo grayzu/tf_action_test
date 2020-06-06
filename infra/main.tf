695617929b23409c4cec1ea0c1b550293b66d6b5
Mark Gray (MSFT) <2914336+grayzu@users.noreply.github.com>
2020-06-06 09:29:44 -0700

    Removing ACR

--- infra/main.tf
+++ infra/main.tf
@@ -3,10 +3,10 @@ resource "azurerm_resource_group" "demo01" {
   location = "eastus2"
 }
 
-resource "azurerm_container_registry" "demo01" {
-  name                = "ameDemoRegistry01"
-  resource_group_name = azurerm_resource_group.demo01.name
-  location            = azurerm_resource_group.demo01.location
-  sku                 = "Basic"
-  admin_enabled       = true
-}
+# resource "azurerm_container_registry" "demo01" {
+#   name                = "ameDemoRegistry01"
+#   resource_group_name = azurerm_resource_group.demo01.name
+#   location            = azurerm_resource_group.demo01.location
+#   sku                 = "Basic"
+#   admin_enabled       = true
+# }
